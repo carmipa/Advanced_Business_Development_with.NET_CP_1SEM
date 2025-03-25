05d18c053a6fee103d9b9efe3f5383f10ef21677
carmipa <pabcarminati@gmail.com>
2025-03-25 09:47:56 -0300

    quase pronto

--- cp1/CP1ControleDeProdutos/CP1ControleDeProdutos/Program.cs
+++ cp1/CP1ControleDeProdutos/CP1ControleDeProdutos/Program.cs
@@ -1,2 +1,95 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+using CP1ControleDeProdutos.classes;
+
+namespace CP1ControleDeProdutos
+
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("****************************************************");
+
+        Console.WriteLine("Bem vindo ao gerenciamento de produtos");
+        Console.WriteLine();
+
+        // produto 1
+
+        Produtos p1 = new Produtos();
+
+        Console.WriteLine("Digite os dados do primeiro produto (1):");
+        Console.WriteLine();
+
+        Console.Write("Digite o código do produto...........: ");
+        p1.Codigo = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.Write("Digite o nome do produto.............: ");
+        p1.nomeProduto = Console.ReadLine();
+        Console.Write("Digite a quantidade do produto.......: ");
+        p1.quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.Write("Digite o valor unitário do produto...: ");
+        p1.valorUnitario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.Write("Digite a quantidade de desconto......: ");
+        p1.desconto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.WriteLine();
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("--- Produto 1 ---");
+        Console.WriteLine($"Nome..........: {p1.nomeProduto}");
+        Console.WriteLine($"Quantidade....: {p1.quantidade.ToString("F2", CultureInfo.InvariantCulture)}");
+        Console.WriteLine($"Valor Unitário: {p1.valorUnitario.ToString("F2", CultureInfo.InvariantCulture)}");
+        Console.WriteLine($"Desconto......: {p1.desconto.ToString("F2", CultureInfo.InvariantCulture)}");
+        Console.WriteLine();
+        Console.WriteLine($"O valor do produto sem desconto: {p1.ValorSemDesconto().ToString("F2", CultureInfo.InvariantCulture)}" + " R$");
+        Console.WriteLine($"O valor do produto com desconto: {p1.valorComDesconto().ToString("F2", CultureInfo.InvariantCulture)}" + " R$");
+        Console.WriteLine("------------------------------------------------");
+        Console.ResetColor();
+
+        // produto 2
+
+        
+        Produtos p2 = new Produtos();
+
+        Console.WriteLine("Digite os dados do segundo produto (2):");
+        Console.WriteLine();
+
+        Console.Write("Digite o código do produto...........: ");
+        p2.Codigo = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.Write("Digite o nome do produto.............: ");
+        p2.nomeProduto = Console.ReadLine();
+        Console.Write("Digite a quantidade do produto.......: ");
+        p2.quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.Write("Digite o valor unitário do produto...: ");
+        p2.valorUnitario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.Write("Digite a quantidade de desconto......: ");
+        p2.desconto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.WriteLine();
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("--- Produto 2 ---");
+        Console.WriteLine($"Nome..........: {p2.nomeProduto}");
+        Console.WriteLine($"Quantidade....: {p2.quantidade.ToString("F2", CultureInfo.InvariantCulture)}");
+        Console.WriteLine($"Valor Unitário: {p2.valorUnitario.ToString("F2", CultureInfo.InvariantCulture)}");
+        Console.WriteLine($"Desconto......: {p2.desconto.ToString("F2", CultureInfo.InvariantCulture)}");
+        Console.WriteLine();
+        Console.WriteLine($"O valor do produto sem desconto: {p2.ValorSemDesconto().ToString("F2", CultureInfo.InvariantCulture)}" + " R$");
+        Console.WriteLine($"O valor do produto com desconto: {p2.valorComDesconto().ToString("F2", CultureInfo.InvariantCulture)}" + " R$");
+        Console.WriteLine("------------------------------------------------");
+        Console.ResetColor();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+}
+}
---
+++ cp1/CP1ControleDeProdutos/CP1ControleDeProdutos/classes/Produtos.cs
@@ -0,0 +1,42 @@
+﻿namespace CP1ControleDeProdutos.classes;
+
+public class Produtos
+{
+    public Produtos()
+    {
+    }
+
+    public int Codigo { get; set; }
+    public string nomeProduto { get; set; }
+    public int quantidade { get; set; }
+    public double valorUnitario { get; set; }
+    public double desconto { get; set; }
+    public double valorTotal { get; set; }
+
+    /// <summary>
+    /// Calcula o valor total do produto sem desconto
+    /// </summary>
+    /// <returns></returns>
+    public double ValorSemDesconto()
+    {
+        double totalSemDesconto = quantidade * valorUnitario;
+        return totalSemDesconto;
+    }
+
+    /// <summary>
+    /// Calcula o valor total do produto com desconto
+    /// </summary>
+    /// <returns></returns>
+    public double valorComDesconto()
+    {
+       
+        double totalComDesconto = (quantidade * valorUnitario) - ((quantidade * valorUnitario) * (desconto / 100));
+
+        return totalComDesconto;
+    }
+
+
+
+
+
+}
