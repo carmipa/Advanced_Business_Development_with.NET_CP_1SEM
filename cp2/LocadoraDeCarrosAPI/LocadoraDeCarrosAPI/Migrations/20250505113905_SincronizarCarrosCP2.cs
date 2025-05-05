fc49587d6ce960cd6ace7c609eb6dc07847ee1e9
carmipa <pabcarminati@gmail.com>
2025-05-05 08:44:32 -0300

    ...

---
+++ cp2/LocadoraDeCarrosAPI/LocadoraDeCarrosAPI/Migrations/20250505113905_SincronizarCarrosCP2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LocadoraDeCarrosAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SincronizarCarrosCP2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
